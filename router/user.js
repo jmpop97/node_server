97bd0d328bf805dbcd396ad5fb34568f224e51f8
jmpop <88295468+jmpop@users.noreply.github.com>
2024-04-25 22:48:27 +0900

    refactor user

--- router/user.js
+++ router/user.js
@@ -4,24 +4,50 @@ const router = express.Router();
 const { Op } = require('sequelize');
 const crypto = require('crypto');
 const jwt = require("../modules/jwt")
-const search_user =require('../modules/search_user')
+const search_user =require('../modules/user_search')
+const user_passward = require('../modules/user_password')
 
 router.post("",(req,res)=>{
-    createUser(req,res);
+    //createUser
+    let {id,password,email} = req.body;
+    user_passward.logUp(id,password,email,res)
 })
 
-// router.get("",(req,res)=>{
-//     getUserTable(req,res);
-// })
-
-
 router.get("",(req,res)=>{
-    logIn(req, res);
+    //log in
+    let {id,password} = req.body;
+    user_passward.logIn(id,password,res)
 })
 
 
 router.get("/:id",(req,res)=>{
-    getUserQuery(req, res);
+    //UserQuery
+    let {type,id}=req.body
+    jwt.verify(req.headers.authorization)
+    .then((log_in_user)=>{
+    if (log_in_user.response){
+        res.send(log_in_user)
+        return
+    }
+    switch(type){
+        default:
+            search_user.choice(log_in_user.id,res)
+            return
+        case '*':
+        case 'all':
+            search_user.all(res)
+            return
+        case 'choice':
+            search_user.choice(id,res)
+            return
+        case 'search_init':
+            search_user.search_init(id,res)
+            return
+        case 'search_include':
+            search_user.search_include(id,res)
+            return
+        }
+    })
 })
 
 
@@ -37,89 +63,11 @@ router.delete("/:id",(req,res)=>{
 })
 
 
-function logIn(req, res) {
-    let {id,password} = req.body;
-    if (!id || !password){
-        res.send({"response":400, "error": "wrong input"})
-        return
-    }
-    let hashpassword = crypto.pbkdf2Sync(password,
-        process.env.PASSWORD_SECRET_STRING + id + process.env.PASSWORD_SECRET_STRING,
-        Number(process.env.PASSWORD_SECRET), 64, 'sha512').toString('hex');
-    models.User.findAll({
-        attributes:["id","password"],
-        where: {
-            id:id
-        }
-    })
-    .then((comment) => {
-        if (hashpassword===comment[0].password){
-            const user_data = jwt.sign(comment[0]).then(user_data=>
-                res.send({ "response": 200, "user": user_data})
-            )
-        }
-        else{
-            res.send({ "response": 400, "error":"잘못된 비밀번호"});
-        }        
-        console.log("data is read!");
-    })
-    .catch(error => {
-        console.log("error");
-        res.send({
-            "response": 400,
-            "error": error
-        });
-    });
-}
-
-function createUser(req,res) {
-    let {id,password,email} = req.body;
-    let hashpassword = crypto.pbkdf2Sync(password,
-        process.env.PASSWORD_SECRET_STRING + id + process.env.PASSWORD_SECRET_STRING,
-        Number(process.env.PASSWORD_SECRET), 64, 'sha512').toString('hex');
-    let create_id = {
-        "id": id,
-        "password": hashpassword,
-        "email": email
-    };
-    models.User.create(create_id)
-        .then(_ => {
-            console.log("data is created!");
-            res.send({ "response": 200 });
-        })
-        .catch(error => {
-            console.log({"error":error});
-            res.send({
-                "response": 400,
-                "error": error
-            });
-        });
-}
 
 
-function getUserQuery(req, res) {
-    jwt.verify(req.headers.authorization)
-    .then((log_in_user)=>{
-    if (log_in_user.response){
-        res.send(log_in_user)
-        return
-    }
-    switch(req.body.type){
-        default:
-            search_user.all(res)
-            return
-        case 'choice':
-            search_user.choice(res,req.body.id)
-            return
-        case 'search_init':
-            search_user.search_init(res,req.body.id)
-            return
-        case 'search_include':
-            search_user.search_include(res,req.body.id)
-            return
-    }
-})
-}
+
+
+
 
 function patchUserId(req, res) {
     models.User.update(req.body, {
