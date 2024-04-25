0b2880d4f986a98cee095a0c19df0717c983f449
jmpop <88295468+jmpop@users.noreply.github.com>
2024-04-25 21:06:02 +0900

    search_user type: all, choice,search_init,search_include

---
+++ modules/search_user.js
@@ -0,0 +1,64 @@
+const {User} = require("../models")
+const { Op } = require('sequelize');
+
+module.exports ={
+    all: async (res)=>{
+        const result = User.findAll({
+            attributes:["id"]
+        })
+        .then((comment) => {
+            res.send({ "response": 200, "user": comment });
+        })
+        .catch(error => {
+            console.log({"at":"modules/search_user/all","ids":ids,"error":error});
+            res.send({
+                "response": 400
+            });
+        });
+    },
+    choice: async (res,ids)=>{
+        const result = User.findAll({
+            attributes:["id"],
+            where: {id : ids}
+        })
+        .then((comment) => {
+            res.send({ "response": 200, "user": comment });
+        })
+        .catch(error => {
+            console.log({"at":"modules/search_user/choice","ids":ids,"error":error});
+            res.send({
+                "response": 400
+            });
+        });
+    },
+    search_init: async (res,id)=>{
+        const result = User.findAll({
+            attributes:["id"],
+            where: {id : {[Op.startsWith] : id}}
+        })
+        .then((comment) => {
+            res.send({ "response": 200, "user": comment });
+        })
+        .catch(error => {
+            console.log({"at":"modules/search_user/choice","id":id,"error":error});
+            res.send({
+                "response": 400
+            });
+        });
+    },
+    search_include: async (res,id)=>{
+        const result = User.findAll({
+            attributes:["id"],
+            where: {id : {[Op.substring] : id}}
+        })
+        .then((comment) => {
+            res.send({ "response": 200, "user": comment });
+        })
+        .catch(error => {
+            console.log({"at":"modules/search_user/choice","id":id,"error":error});
+            res.send({
+                "response": 400
+            });
+        });
+    },
+}
