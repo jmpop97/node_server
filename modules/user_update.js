98e32b0e6b2efd86a86b5f1799e396ae04cb60af
jmpop <88295468+jmpop@users.noreply.github.com>
2024-04-26 17:31:21 +0900

    refactor user_update

--- modules/user_update.js
+++ modules/user_update.js
@@ -1,5 +1,31 @@
-class Users {
-    method1(test) {console.log(test)}
+const {User} = require("../models")
+const { Op } = require('sequelize');
+
+module.exports ={
+    patch: async (id,patch_data,res)=>{
+
+        let patch_datas={email:patch_data.email}
+        for (var key in patch_datas){
+            if (patch_datas[key]==null){
+                delete patch_datas[key]
+            }
+        }
+
+        console.log(patch_datas)
+        User.update(patch_datas, {
+            where: {
+                id: id
+            }
+        })
+            .then((comment) => {
+                console.log("data is update");
+                res.send({ "response": 200});
+            })
+            .catch(error => {
+                console.log({"at":"moduler/user_update/patch","input":{"id":id,"patch_data":patch_data},"error":error});
+                res.send({
+                    "response": 400,
+                });
+            });
+    },
 }
-var user=new Users
-module.exports=(sequelize, DataTypes)=>{user}
