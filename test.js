05951f4fb116f0cad971590f6bb89f4ac5723c87
jmpop <88295468+jmpop@users.noreply.github.com>
2024-04-26 14:40:18 +0900

    require class test

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
-const crypto = require('crypto');
-const string = 'this is my password'
-const hashPassword =crypto.createHash('sha512').update(string).digest("hex")
-console.log(hashPassword)
-const hashedPw = crypto.pbkdf2Sync(string, "", 1, 64, 'sha512').toString('hex');
-console.log(hashedPw)
-const hashedPw2 = crypto.pbkdf2Sync(hashedPw, "", 1, 64, 'sha512').toString('hex');
-console.log(hashedPw2)
+const {Users} = require("./modules/user_update")
+// var user= new Users
+Users.method1("test")
