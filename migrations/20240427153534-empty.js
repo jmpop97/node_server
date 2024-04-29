e5f2d2f73454b9a48610de0c413b12260abb328b
jmpop <88295468+jmpop@users.noreply.github.com>
2024-04-29 21:25:42 +0900

    refactor restart

--- migrations/20240427153534-empty.js
+++ migrations/20240427153534-empty.js
@@ -3,6 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    queryInterface.dropTable('Users')
     /**
      * Add altering commands here.
      *
