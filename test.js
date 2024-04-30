864ccab25510109ea85fe34bd5e479ee3d85aa14
jmpop <88295468+jmpop@users.noreply.github.com>
2024-04-30 20:55:09 +0900

    UserInfo create combine User create

--- test.js
+++ test.js
@@ -1,31 +1,40 @@
-const {Status,User,Permission,} = require('./models')
-// const result = Status.findAll({attributes:["stateName"],
-//     include:{
-//         model:User,
-//     }
+const {Status,User,Permission,UserPermission,UserInfo} = require('./models')
+
+// User.findAll({
+//     attributes:["id","password","state","email"],
+//     where: {
+//         id:"id1"
+//     },
+//         include:[{
+//             model:Status,
+//             attributes:['stateName']
+//     },
+//     {
+//         attributes:['authName'],
+//         model:Permission,
+//         through: {
+//             attributes: [],
+//           },
+//     }]
 // })
 // .then((comment) => {
 // console.log(JSON.stringify(comment, null, 2))
 // })
 
-User.findAll({
-    attributes:["id","password","state","email"],
-    where: {
-        id:"id1"
-    },
-        include:[{
-            model:Status,
-            attributes:['stateName']
-    },
-    {
-        attributes:['authName'],
-        model:Permission,
-        through: {
-            attributes: [],
-          },
-    }]
-
-})
+id="id-5"
+email=id+"@m.com"
+input={id:id,
+password:"0",
+email:email,
+UserInfo:{userId:id}}
+User.create(input,{include:[UserInfo]})
 .then((comment) => {
 console.log(JSON.stringify(comment, null, 2))
 })
+// User.findOne({
+//     where: {id:'id1'},
+//     include:Userinfo
+// })
+// .then((comment) => {
+// console.log(JSON.stringify(comment, null, 2))
+// })
