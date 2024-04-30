274b7cba33da09be6c63def1b03595cb24066c60
jmpop <88295468+jmpop@users.noreply.github.com>
2024-04-30 09:35:22 +0900

    중간테이블 associate 정리

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
 const {Status,User,Permission,} = require('./models')
-const result = Status.findAll({attributes:["stateName"],
-    include:{
-        model:User,
-    }
-})
-.then((comment) => {
-console.log(JSON.stringify(comment, null, 2))
-})
+// const result = Status.findAll({attributes:["stateName"],
+//     include:{
+//         model:User,
+//     }
+// })
+// .then((comment) => {
+// console.log(JSON.stringify(comment, null, 2))
+// })
 
 const result1 = User.findAll({attributes:["id"],
     include:{
         model:Permission,
-    }
+        through: {
+            attributes: [],
+          },
+    },
 })
 .then((comment) => {
 console.log(JSON.stringify(comment, null, 2))
