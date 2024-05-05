e41173f4e28a93cf07fd9c5b5bcb4c5cec5cf557
jmpop <88295468+jmpop@users.noreply.github.com>
2024-05-05 21:28:46 +0900

    permission change with cache data

--- cache_DB/permission.js
+++ cache_DB/permission.js
@@ -5,30 +5,27 @@ async function get(i){
     value = cache_permission.get(i)
     if (value==undefined){
         perms = await Permission.findAll({where:{authName:i}})
-        console.log(perms)
         for(j in perms){
             cache_permission.set(perms[j].authName,perms[j].authId)
         }
         value = cache_permission.get(i)
     }
-    return {response:200,value:value}
+    return value
 }
 
 
 async function patch(i){
     perms = await Permission.findAll({where:{authName:i}})
     for(j in perms){
-        console.log("save")
         cache_permission.set(perms[j].authName,perms[j].authId)
     }
     value = cache_permission.get(i)
-    return {response:200,value:value}
+    return value
 }
 
 async function setAll(){
     perms = await Permission.findAll({})
     for(j in perms){
-        console.log("save")
         cache_permission.set(perms[j].authName,perms[j].authId)
     }
     return perms
