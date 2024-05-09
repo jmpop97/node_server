a99853476ee999fad4ef14c6ec659a2eb4ffa987
jmpop <88295468+jmpop@users.noreply.github.com>
2024-05-09 17:20:37 +0900

    ip log at all request

--- router/all_api.js
+++ router/all_api.js
@@ -1,9 +1,16 @@
 let express = require("express")
 const router = express.Router();
 const {UserIPLog} = require("../models")
+const jwt = require("../modules/jwt")
 
-router.use("", (req, res, next)=>{
-    UserIPLog.create({IP:req.ip,url:req.method+req.url})
+
+router.use("", async (req, res, next)=>{
+    let log_in_user = await jwt.verify(req.headers.authorization)
+    add={IP:req.ip,
+        userId:log_in_user.id,
+        url:req.method+req.url}
+    console.log(add)
+    UserIPLog.create(add)
     next();
   });
 
