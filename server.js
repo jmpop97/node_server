484b8d81002d3626b4853419a2143e0cd899973d
jmpop97 <pjm970128@gmail.com>
2024-05-13 15:55:48 +0900

    example of image request

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ const express = require('express')
 const app = express()
 const userRouter= require("./router/user")
 const permissionRouter= require("./router/permission")
-const errorMessage =require("./router/error_message")
+const errorMessageRouter =require("./router/error_message")
+const articleRouter = require("./router/article")
 const all_api =require("./router/all_api")
-const {UserIPLog} = require("./models")
+
 
 app.set('port',port_num)
 app.use(express.json())
+app.use(express.urlencoded());
 app.use('/', all_api);
-
-
 
 app.use('/user',userRouter)
 app.use('/permission',permissionRouter)
-app.use('/error_message',errorMessage)
+app.use('/error_message',errorMessageRouter)
+app.use('/article',articleRouter)
 
 
 app.listen(app.get('port'),()=>{
