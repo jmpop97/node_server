4be08f5032181e8c649613572525569d38d739b2
jmpop97 <pjm970128@gmail.com>
2024-06-09 20:47:50 +0900

    split study migrate

--- test.js
+++ test.js
@@ -1,51 +1,43 @@
-require('dotenv').config();
-const user = require("./modules/user")
-const seed_db=require("./modules/seedDB")
-const models=require("./models")
-const nodemailer = require('nodemailer');
-const fs = require('node:fs');  
-const cheerio = require('cheerio')
-const {Email} = require('./modules/send_email')
-const User =require('./modules/user')
-
-email='canwim00@gmail.com'
-type="User"
-key="aae2a85a5c5f35e2078be83852b1256aa0b57344"
-// new User.Authenfication({email,type}).AuthenficationCreate()
-new User.Authenfication({email,type,key}).AuthenficationAfter()
-
-// new User.Authenfication({email:email}).AuthenficationAfter()
-
-// var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.google_id,
-//     pass: process.env.google_email
-//   }
-// });
+// require('dotenv').config();
+// const user = require("./modules/user")
+// const seed_db=require("./modules/seedDB")
+// const models=require("./models")
+// const nodemailer = require('nodemailer');
+// const fs = require('node:fs');  
+// const cheerio = require('cheerio')
+// const {Email} = require('./modules/send_email')
+// const User =require('./modules/user')
 
 
+const { Sequelize } = require('sequelize');
 
+const sequelize = new Sequelize(
+    // database: 'NodeServer',
+    // user: 'jmpop',
+    // password: 'node3.141592',    
+    'NodeServer',
+    'jmpop',
+    'node3.141592',
+{
+  dialect: 'postgres',
+  host: 'node-server.ct4i8qmg4xyk.ap-northeast-2.rds.amazonaws.com',
+  port: 5432,
+  dialectOptions: {
+    ssl: {
+      require: true, // SSL 사용을 강제
+      rejectUnauthorized: false, // 자체 서명된 인증서 허용
+    },
+  }
+  // ssl: true,
+//   clientMinMessages: 'notice',
+});
+async function Test(){
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
 
-
-// async function test(){
-//   let title="제목"
-//   let body="내용"
-//   const mailOptions = {
-//     from: 'pjm970128@gmail.com', // 작성자
-//     to: 'canwim00@gmail.com', // 수신자
-//     subject: '동적 이메일', // 메일 제목
-//     text: 'hi',
-//     attachments:[{
-//     }]
-//   };
-//   transporter.sendMail(mailOptions, function(error, info){
-//       if (error) {
-//           console.log(error);
-//         } else {
-//             console.log('Email sent: ' + info.response);
-//           }
-//         });
-// }
-      
-// test()
+Test()
